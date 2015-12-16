e24f02fc898cd1b0585417ddb4f994616ddf711f
Lerenwe <johan.nagy.contact@gmail.com>
2015-12-16 14:06:18 +0100

    Border Detection Camera

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    #region move variables
+    Vector3 targetPosition = Vector3.zero;
+    #endregion
+
+    #region Components & Objects Variable
+    public SpriteRenderer currentBackGround;
+    public GameObject player;
+    Camera camera;
+    Rect CameraRect;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        #region setting up components & objects
+        camera = gameObject.GetComponent<Camera>();
+        CameraRect = camera.pixelRect;
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        targetPosition = new Vector3(player.transform.position.x, currentBackGround.transform.position.y, -10f);
+
+        PreventGettingOutOfBackground();
+
+        transform.position = targetPosition;
+    }
+
+    void PreventGettingOutOfBackground ()
+    {
+        Vector3 currentBackGroundLeftBorder = currentBackGround.transform.position - currentBackGround.sprite.bounds.extents;
+
+        Vector3 cameraRightBorder = camera.ScreenToWorldPoint(new Vector3(CameraRect.width, CameraRect.height / 2, 0));
+        Vector3 currentBackGroundRightBorder = currentBackGround.transform.position + currentBackGround.sprite.bounds.extents;
+
+        float cameraHorizontalExtent = cameraRightBorder.x - transform.position.x;
+
+        //Debug.DrawLine(camera.transform.position, new Vector3(camera.transform.position.x + cameraHorizontalExtent, 0, 0));
+
+        if ( (targetPosition.x - cameraHorizontalExtent < currentBackGroundLeftBorder.x) || (targetPosition.x + cameraHorizontalExtent > currentBackGroundRightBorder.x) )
+            targetPosition.x = transform.position.x;
+    }
+}
