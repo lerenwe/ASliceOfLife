2cf052b0106056d5d3a80a95b4f9cd28bb68a448
Lerenwe <johan.nagy.contact@gmail.com>
2015-12-23 01:08:00 +0100

    Parallax BEEYATCH

---
+++ Assets/Parallax.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+    public GameObject mainBackGroundObject;
+    SpriteRenderer mainBackGroundSprite;
+    SpriteRenderer currentActiveBackGround;
+    SpriteRenderer thisRenderer;
+
+    public float offset = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        thisRenderer = this.GetComponent<SpriteRenderer>();
+
+        if (mainBackGroundObject == null)
+            Debug.LogError(gameObject.name + "parallax Script need to get main Background!");
+        else
+        {
+            mainBackGroundSprite = mainBackGroundObject.GetComponent<SpriteRenderer>();
+        }
+
+        
+    }
+
+    void Awake ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentActiveBackGround = GameStateManager.currentActiveScene.GetComponent<SubSceneManager>().myBackGround.GetComponent<SpriteRenderer>();
+
+        if (currentActiveBackGround == mainBackGroundSprite)
+        {
+            Vector3 mainBackGroundCenter = mainBackGroundObject.transform.position;
+
+            float distBetweenBGandCamera = mainBackGroundObject.transform.position.x - Camera.main.transform.position.x;
+
+            if (offset <= 1)
+                transform.position = new Vector3 ( ((mainBackGroundObject.transform.position.x - distBetweenBGandCamera) * offset), transform.position.y, transform.position.z);
+            else
+                transform.position = new Vector3(((mainBackGroundObject.transform.position.x + distBetweenBGandCamera) * offset), transform.position.y, transform.position.z);
+        }
+
+	}
+}
---
+++ Assets/Scripts/subSceneExit.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class subSceneExit : MonoBehaviour {
+
+    public GameObject destinationPoint;
+
+    [SerializeField]
+    bool touchToExit;
+    bool playerIsTouchingExit = false;
+
+    ChoiceLoader choiceLoader;
+
+    [SerializeField]
+    List<Choice> preRequisiteChoices = new List<Choice>();
+
+    // Use this for initialization
+    void Start () {
+        choiceLoader = GameObject.Find("GameStateManager").GetComponent<ChoiceLoader>();
+        //preRequisiteChoices = choiceLoader.everyChoices.choices;
+
+        Debug.Log(preRequisiteChoices.GetHashCode());
+        Debug.Log(choiceLoader.everyChoices.choices.GetHashCode());
+
+        if (preRequisiteChoices.Count < choiceLoader.everyChoices.choices.Count)
+        {
+            foreach (Choice choice in choiceLoader.everyChoices.choices)
+            {
+                preRequisiteChoices.Add(choice);
+            }
+        }
+	}
+
+    void Awake ()
+    {
+        #region check if mandatory components & objects are present
+        if (destinationPoint == null)
+            Debug.LogError(gameObject.name + " needs a destination point!");
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if (!touchToExit && playerIsTouchingExit && Input.GetAxisRaw("Vertical") > .5f)
+        {
+            Debug.Log("Trying to exit...");
+
+            if (preRequisiteChoices.SequenceEqual(choiceLoader.everyChoices.choices))
+            { 
+                GameStateManager.player.transform.position = destinationPoint.transform.position;
+                Debug.Log("Exit right now");
+            }
+        }
+
+        if (playerIsTouchingExit)
+            Debug.Log("Player is colliding with " + gameObject.name);
+	}
+
+    void OnTriggerEnter2D (Collider2D hit)
+    {
+        if (touchToExit)
+        {
+            if (hit == GameStateManager.playerCollider)
+                GameStateManager.player.transform.position = destinationPoint.transform.position;
+        }
+    }
+
+    void OnTriggerStay2D (Collider2D hit)
+    {
+        if (!touchToExit)
+        {
+            if (hit == GameStateManager.playerCollider)
+                playerIsTouchingExit = true;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D hit)
+    {
+        if (!touchToExit)
+        {
+            if (hit == GameStateManager.playerCollider)
+                playerIsTouchingExit = false;
+        }
+    }
+}
