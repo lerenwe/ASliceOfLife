431a8aecae14029a5bf39c3e561c2bbcff6a4863
Lerenwe <johan.nagy.contact@gmail.com>
2016-03-09 00:32:11 +0100

    New text display System \o/ (To clean up next ^^)

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dialogue : MonoBehaviour {
+
+    public string textToDisplay = "lol";
+    public GameObject wordPrefab;
+    public GameObject mainCanvas;
+    string displayedText = null;
+    char[] currentCharsToDisplay;
+    string[] currentWordsToDisplay;
+    float nextCharacterTimer;
+    public float displaySpeedRate = 1f;
+    int i = 0;
+    RectTransform justSpawnedRectTransform;
+
+    Text textComponent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        textComponent = this.GetComponent<Text>();
+        DisplayNewText(textToDisplay);
+        justSpawnedRectTransform = this.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       nextCharacterTimer += Time.deltaTime;
+
+        if (nextCharacterTimer > displaySpeedRate && i < currentWordsToDisplay.Length)
+        {
+            GameObject spawnedNewWord = Object.Instantiate(wordPrefab) as GameObject;
+
+            Text newWordText = spawnedNewWord.GetComponent<Text>();
+            newWordText.text += currentWordsToDisplay[i];
+            spawnedNewWord.transform.name = currentWordsToDisplay[i];
+            newWordText.text += " ";
+            spawnedNewWord.GetComponent<ContentSizeFitter>().SetLayoutHorizontal();
+            spawnedNewWord.GetComponent<ContentSizeFitter>().SetLayoutVertical();
+            spawnedNewWord.GetComponent<RectTransform>().position = justSpawnedRectTransform.position + new Vector3(justSpawnedRectTransform.rect.xMax + spawnedNewWord.GetComponent<RectTransform>().rect.size.x / 2, 0, 0);
+
+            spawnedNewWord.transform.SetParent(mainCanvas.transform);
+
+            i++;
+            nextCharacterTimer = 0;
+            justSpawnedRectTransform = spawnedNewWord.GetComponent<RectTransform>();
+        }
+	}
+    
+    void DisplayNewText(string newText)
+    {
+        //currentCharsToDisplay =  textToDisplay.ToCharArray();
+        currentWordsToDisplay = textToDisplay.Split(' ');
+    }
+}
---
+++ Assets/WordNextToFirstTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WordNextToFirstTest : MonoBehaviour {
+
+    //public RectTransform otherWord;
+    RectTransform mahRectTransform;
+    Vector3 startPos = Vector3.zero;
+    bool begin = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mahRectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    void Awake ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        if(begin)
+        {
+            Debug.Log(this.name + " " + mahRectTransform.position);
+            startPos = mahRectTransform.position;
+            mahRectTransform.position = new Vector3(mahRectTransform.position.x, mahRectTransform.position.y + 10, mahRectTransform.position.z);
+            Debug.Log(this.name + " " + mahRectTransform.position);
+            begin = false;
+        }
+
+        Vector3 velocityNow = Vector3.zero;
+        mahRectTransform.position = Vector3.SmoothDamp(mahRectTransform.position, startPos, ref velocityNow, .08f);
+        Debug.Log(this.name + " " + mahRectTransform.position);
+        //mahRectTransform.position = otherWord.position + new Vector3(otherWord.rect.xMax + mahRectTransform.rect.size.x / 2, 0, 0);
+    }
+}
