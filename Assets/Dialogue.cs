b3706265bf3dfb3dd6c8aec24347bce6baa15795
Lerenwe <johan.nagy.contact@gmail.com>
2016-05-29 20:13:33 +0200

    Dialogues now functionnal! Polish needed.

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -11,42 +11,59 @@ public class Dialogue : MonoBehaviour {
     public string[] dialogueLines;
     public int[] characterSpeaking;
     public GameObject[] dialogueCharacters;
+    public bool dialogueClosed = false;
 
     int currentLineToDisplay = 0;
 
     DialogueDisplayer dialogueDisplayer;
+    public GameObject dialogueDisplayObject;
 
 	// Use this for initialization
-	void Start () {
-        dialogueDisplayer = GameObject.Find("DialogueDisplay").GetComponent<DialogueDisplayer>();
+	void Start ()
+    {
+        dialogueDisplayer = dialogueDisplayObject.GetComponent<DialogueDisplayer>();
         if (dialogueLines.Length != characterSpeaking.Length)
             Debug.LogError("THE NUMBER OF DIALOGUE LINES DOES NOT MATCH THE NUMBER OF CHARACTER SPEAKING");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	 if (dialogueTriggered ||(dialogueInProgress && Input.GetKeyDown ("space")))
+	void Update ()
+    {
+        if (!dialogueClosed)
         {
-            Vector3 characterPosition = Camera.main.WorldToScreenPoint(dialogueCharacters[characterSpeaking[currentLineToDisplay]].GetComponent<Collider2D>().bounds.max);
+            if (dialogueTriggered || (dialogueInProgress && Input.GetKeyDown("space")))
+            {
+                dialogueDisplayObject.SetActive(true);
+
+                if (dialogueDisplayObject == null && dialogueDisplayObject.activeInHierarchy)
+                    dialogueDisplayer = dialogueDisplayObject.GetComponent<DialogueDisplayer>();
+
+                Vector3 characterPosition = Camera.main.WorldToScreenPoint(dialogueCharacters[characterSpeaking[currentLineToDisplay]].GetComponent<Collider2D>().bounds.max);
 
 
-            dialogueDisplayer.bubbleBackRectTransform.position = new Vector3 (characterPosition.x + dialogueDisplayer.bubbleBackRectTransform.GetComponent<Image>().rectTransform.rect.width / 2, characterPosition.y, characterPosition.z);
+                dialogueDisplayer.bubbleBackRectTransform.position = new Vector3(characterPosition.x + dialogueDisplayer.bubbleBackRectTransform.GetComponent<Image>().rectTransform.rect.width / 2, characterPosition.y, characterPosition.z);
 
-            dialogueDisplayer.textToDisplay = dialogueLines[currentLineToDisplay];
-            dialogueDisplayer.ResetDialogueBubble();
-            dialogueTriggered = false;
-            dialogueInProgress = true;
+                dialogueDisplayer.textToDisplay = dialogueLines[currentLineToDisplay];
+                dialogueDisplayer.ResetDialogueBubble();
+                dialogueTriggered = false;
+                dialogueInProgress = true;
 
-            if (currentLineToDisplay < dialogueLines.Length - 1)
-                currentLineToDisplay++;
-            else
-                dialogueInProgress = false;
+                if (currentLineToDisplay < dialogueLines.Length - 1)
+                    currentLineToDisplay++;
+                else
+                {
+                    dialogueInProgress = false;
+                    Debug.Log("Dialogue is over");
+                }
 
 
-        }
-     else if (!dialogueInProgress && Input.GetKeyDown("space"))
-        {
-            Debug.Log("End of Dialogue");
+            }
+            else if (!dialogueInProgress && Input.GetKeyDown("space"))
+            {
+                Debug.Log("End of Dialogue");
+                dialogueDisplayObject.SetActive(false);
+                dialogueClosed = true;
+            }
         }
 	}
 }
--- Assets/SoccerSequenceManager.cs
+++ Assets/SoccerSequenceManager.cs
@@ -13,6 +13,8 @@ public class SoccerSequenceManager : MonoBehaviour {
     public bool ballShot = false;
     public bool sequenceStartTriggered = false;
     public bool ballDisappeared;
+    public bool waitingForDialogueEnd = false;
+    public GameObject thisDialogue;
 
     [SerializeField]
     GameObject secondTargetPoint;
@@ -42,19 +44,29 @@ public class SoccerSequenceManager : MonoBehaviour {
             soccerKidScript.moveTowardPlayer = true;
         }
 
-        if (soccerKidScript != null && soccerKidScript.onDestinationPoint)
+        if (waitingForDialogueEnd && thisDialogue.GetComponent<Dialogue>().dialogueClosed)
         {
-            //Bon là faudra lancer le dialogue et tout freezer tant qu'il est pas fini, on peut peut être faire ça avec un yield... A voir !
-            ballDisappeared = false;
-            soccerKidScript.moveTowardPlayer = false;
+            waitingForDialogueEnd = false;
+
             Debug.Log("Going to point...");
             soccerKidScript.moveTowardTargetPoint = true;
 
-            if (!soccerKidScript.GetComponent<Renderer>().isVisible)
+            if (soccerKid != null && !soccerKidScript.GetComponent<Renderer>().isVisible)
             {
                 Destroy(soccerKid.gameObject);
                 playerScript.canControl = true;
             }
+        }
+
+        if (soccerKidScript != null && soccerKidScript.onDestinationPoint)
+        {
+            //Bon là faudra lancer le dialogue et tout freezer tant qu'il est pas fini, on peut peut être faire ça avec un yield... A voir !
+            thisDialogue.GetComponent<Dialogue>().dialogueTriggered = true;
+            Debug.Log("TRIGGERED HOLY SHIT");
+            ballDisappeared = false;
+            soccerKidScript.moveTowardPlayer = false;
+            waitingForDialogueEnd = true;
+            soccerKidScript.onDestinationPoint = false;
         }
 
 
